1ba869f8b437b3fabde42d332c17eb505516b598
Irfan Samad <irfansamad2002@gmail.com>
2023-06-28 11:46:42 +0800

    playerpref save using AddHighscoreEntry, fix no playerpref issue, update score base on name
    now need to do delete/ clear the leaderboard without deleteing the playerpref

--- Assets/Scripts/Test Scripts/HighScoreTable.cs
+++ Assets/Scripts/Test Scripts/HighScoreTable.cs
@@ -4,47 +4,76 @@ using UnityEngine;
 using TMPro;
 public class HighScoreTable : MonoBehaviour
 {
-    private Transform entrycontainer;
-    private Transform entrytemplate;
+    [SerializeField] private Transform entrycontainer;
+    [SerializeField] private Transform entrytemplate;
     private List<Transform> highscoreEntryTransformList;
+
+    const string PLAYERPREFDATABASE = "highscoreTable";
     private void Awake()
     {
-        entrycontainer = transform.Find("highScoreEntryContainer");
-        entrytemplate = entrycontainer.Find("highscoreEntryTemplate");
-
+        //Turn off the template
         entrytemplate.gameObject.SetActive(false);
 
-        //AddHighscoreEntry(1000, "newGuy");
-
-        string jsonString = PlayerPrefs.GetString("highscoreTable");
+      
+        
+        //Get the data
+        string jsonString = PlayerPrefs.GetString(PLAYERPREFDATABASE);
         HighScores highscores = JsonUtility.FromJson<HighScores>(jsonString);
 
-        //sort entry based on score
-        for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
-        {
-            for (int j = i; j < highscores.highscoreEntryList.Count; j++)
-            {
-                if (highscores.highscoreEntryList[j].score > highscores.highscoreEntryList[i].score)
-                {
-                    //swap
-                    HighscoreEntry temp = highscores.highscoreEntryList[i];
-                    highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
-                    highscores.highscoreEntryList[j] = temp;
 
+
+
+        //no nd check if there is no data
+        if (CheckIfPlayerPrefSet())
+        {
+            Debug.Log("has highscoretable");
+            //sort entry based on score
+            for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
+            {
+                for (int j = i + 1; j < highscores.highscoreEntryList.Count; j++)
+                {
+                    if (highscores.highscoreEntryList[j].score > highscores.highscoreEntryList[i].score)
+                    {
+                        //swap
+                        HighscoreEntry temp = highscores.highscoreEntryList[i];
+                        highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
+                        highscores.highscoreEntryList[j] = temp;
+
+                    }
                 }
             }
+
+
+            highscoreEntryTransformList = new List<Transform>();
+            foreach (HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
+            {
+                CreateHighScoreEntryTransform(highscoreEntry, entrycontainer, highscoreEntryTransformList);
+            }
         }
-
-
-        highscoreEntryTransformList = new List<Transform>();
-        foreach (HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
+        else
         {
-            CreateHighScoreEntryTransform(highscoreEntry, entrycontainer, highscoreEntryTransformList);
+            Debug.Log("dont have highscoretable");
         }
+
+
+
+
+       
 
 
     }
 
+    private bool CheckIfPlayerPrefSet()
+    {
+        if (PlayerPrefs.HasKey(PLAYERPREFDATABASE))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     private void CreateHighScoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
     {
         float templateHeight = 30f;
@@ -81,27 +110,100 @@ public class HighScoreTable : MonoBehaviour
 
     private void AddHighscoreEntry(int score, string name)
     {
+        string jsonString;
+        HighScores highscores;
+
         //Create HighscoreEntry
         HighscoreEntry highscoreEntry = new HighscoreEntry { score = score, name = name };
 
-        //Load saved Highscores
-        string jsonString = PlayerPrefs.GetString("highscoreTable");
-        HighScores highscores = JsonUtility.FromJson<HighScores>(jsonString);
+       
+        
+        if (CheckIfPlayerPrefSet())//Check if there is data in playerpref
+        {
+            //Load saved Highscores
+            jsonString = PlayerPrefs.GetString("highscoreTable");
+            highscores = JsonUtility.FromJson<HighScores>(jsonString);
 
-        //Add new entry to Highscores
-        highscores.highscoreEntryList.Add(highscoreEntry);
+
+            //check if name already exist in database
+            if (CheckNameInDatabase(name, highscores))
+            {
+                UpdateScore(name, score, highscores);
+            }
+            else
+            {
+                //Add new entry to Highscores
+                highscores.highscoreEntryList.Add(highscoreEntry);
+            }
+
+
+
+
+            //Save updated Highscores
+            string json = JsonUtility.ToJson(highscores);
+            PlayerPrefs.SetString("highscoreTable", json);
+            PlayerPrefs.Save();
+
+        }
+        else //if there is no data in playerpref
+        {
+            Debug.Log("trying to add without player pref being set");
+            List<HighscoreEntry> highscoreEntrylist = new List<HighscoreEntry>();
+            HighscoreEntry tempHighscoreEntry = new HighscoreEntry { score = score, name = name };
+            highscoreEntrylist.Add(tempHighscoreEntry);
+
+            HighScores tempHighscores = new HighScores { highscoreEntryList = highscoreEntrylist };
+            string json = JsonUtility.ToJson(tempHighscores);
+            PlayerPrefs.SetString("highscoreTable", json);
+            PlayerPrefs.Save();
+        }
+    }
+
+
+    private bool CheckNameInDatabase(string name, HighScores highscores)
+    {
+        //check if name already exist
+        for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
+        {
+            if (highscores.highscoreEntryList[i].name == name)
+            {
+                Debug.Log("name exist in the database");
+
+                return true;
+            }
+        }
+        Debug.Log("no name exist in the database");
+        return false;
+
+    }
+
+
+    private void UpdateScore(string name, int score, HighScores highscores)
+    {
+        
+        string findName = name;
+        int newScore = score;
+        for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
+        {
+            if (highscores.highscoreEntryList[i].name == findName)
+            {
+                highscores.highscoreEntryList[i].score = newScore;
+
+            }
+        }
 
         //Save updated Highscores
         string json = JsonUtility.ToJson(highscores);
         PlayerPrefs.SetString("highscoreTable", json);
         PlayerPrefs.Save();
-
     }
 
     private class HighScores
     {
-        public List<HighscoreEntry> highscoreEntryList; 
+        public List<HighscoreEntry> highscoreEntryList;
     }
+
+
 
     [System.Serializable]
     private class HighscoreEntry
