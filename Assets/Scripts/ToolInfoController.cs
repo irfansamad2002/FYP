f3b25ca5e5a04bd780d650e7fcd462399e620f9d
Irfan Samad <irfansamad2002@gmail.com>
2023-07-12 17:21:25 +0800

    built for fon Tool info diff

--- Assets/Scripts/ToolInfoController.cs
+++ Assets/Scripts/ToolInfoController.cs
@@ -30,8 +30,8 @@ public class ToolInfoController : MonoBehaviour
     public void LoadContent(DentistTool dentistTool)
     {
         toolNameText.text = dentistTool.Name;
-        toolDescText.text = dentistTool.Usage + "\n" + dentistTool.Instrumentation + "\n" + dentistTool.InstrumentGrasp;
         toolImage.sprite = dentistTool.Icon;
+        toolDescText.text = dentistTool.Usage + "\n" + dentistTool.Instrumentation + "\n" + dentistTool.InstrumentGrasp;
         //Debug.Log("ToolInfo loaded with " + dentistTool.Name);
     }
 
