30d0d4c62b61a61890f93a28273365c1e01c652c
tanmarcus <tanmarcus16@gmail.com>
2023-07-13 10:55:26 +0800

    prefab stuff

--- Assets/Scripts/ARSpawnManager.cs
+++ Assets/Scripts/ARSpawnManager.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class ARSpawnManager : MonoBehaviour
 {
+    public GameObject BlockerGameObject;
     public GameObject dragTutorial;
     public GameObject pinchTutorial;
     public GameObject twistTutorial;
-    public GameObject BlockerGameObject;
     private bool shownDragGuide;
     private bool shownPinchGuide;
     private bool shownTwistGuide;
--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -25,7 +25,7 @@ public class SceneChanger : MonoBehaviour
     public void ChangeToSpawnScene()
     {
         //SceneManager.LoadScene("AR Spawn Scene");
-        SceneManager.LoadScene("AR Spawn Scene");
+        SceneManager.LoadScene("MarcusARTest");
     }
     public void ChangeToVideoScene()
     {
