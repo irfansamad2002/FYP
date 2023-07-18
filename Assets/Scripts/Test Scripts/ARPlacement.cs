81e8a35e7af9854d142970305aa591f4f6ffdd11
tanmarcus <tanmarcus16@gmail.com>
2023-07-18 11:24:25 +0800

    spawned object text and buttons for scale/rotate

--- Assets/Scripts/Test Scripts/ARPlacement.cs
+++ Assets/Scripts/Test Scripts/ARPlacement.cs
@@ -18,6 +18,11 @@ public class ARPlacement : MonoBehaviour
 
     [Header("Rotation Buttons")]
     [SerializeField] private GameObject rotationButtons;
+    //[SerializeField] private TMP_Text rotationButtonText;
+
+    [Header("Text Info")]
+    [SerializeField] private TMP_Text RSText;
+    [SerializeField] private TMP_Text XYZText;
 
     private GameObject spawnedObject;
 
@@ -27,6 +32,13 @@ public class ARPlacement : MonoBehaviour
 
     private LeanTwistRotateAxis leanTwistRotateAxis;
     private LeanPinchScale leanPinchScale;
+
+    
+
+    //private int xyzIndex = 0;
+    //private bool xAxis;
+    //private bool yAxis;
+    //private bool zAxis;
 
     void Start()
     {
@@ -47,6 +59,9 @@ public class ARPlacement : MonoBehaviour
         // start with rotate first
         RSButton.SetActive(true);
         rotationButtons.SetActive(true);
+
+        // start on y axis
+        //xyzIndex = 0; // 0 for y; 1 for z; 2 for x
     }
 
 
@@ -67,17 +82,21 @@ public class ARPlacement : MonoBehaviour
             if (leanPinchScale.enabled == true)
             {
                 rotationButtons.SetActive(false);
-                RSButtonText.text = "Rotate";
+                //RSButtonText.text = "Rotate";
+                RSText.text = "Scale";
+                XYZText.enabled = false;
             }
             else
             {
                 rotationButtons.SetActive(true);
-                RSButtonText.text = "Scale";
+                //RSButtonText.text = "Scale";
+                RSText.text = "Rotate";
+                XYZText.enabled = true;
+
             }
         }
        
     }
-
     public void OnRSButtonPressed()
     {
         leanPinchScale.enabled = !leanPinchScale.enabled;
@@ -87,19 +106,53 @@ public class ARPlacement : MonoBehaviour
         Debug.Log("leanTwistRotateAxis: " + leanTwistRotateAxis.enabled);
     }
     #region rotation button functions
+    //public void OnRotationButtonPressed()
+    //{
+    //    xyzIndex += 1;
+
+    //    if (xyzIndex > 2)
+    //    {
+    //        xyzIndex = 0;
+    //    }
+
+    //    if (xyzIndex == 0)
+    //    {
+    //        // y axis
+    //        spawnedObject.GetComponent<LeanTwistRotateAxis>().ChangeAxis(new Vector3(0f, -1f, 0f));
+    //        Debug.Log(spawnedObject.GetComponent<LeanTwistRotateAxis>().Axis);
+    //        rotationButtonText.text = "Z";
+    //    }
+    //    else if (xyzIndex == 1)
+    //    {
+    //        // z axis
+    //        spawnedObject.GetComponent<LeanTwistRotateAxis>().ChangeAxis(new Vector3(0f, 0f, 1f));
+    //        Debug.Log(spawnedObject.GetComponent<LeanTwistRotateAxis>().Axis);
+    //        rotationButtonText.text = "X";
+    //    }
+    //    else if (xyzIndex == 2)
+    //    {
+    //        // x axis
+    //        spawnedObject.GetComponent<LeanTwistRotateAxis>().ChangeAxis(new Vector3(-1f, 0f, 0f));
+    //        Debug.Log(spawnedObject.GetComponent<LeanTwistRotateAxis>().Axis);
+    //        rotationButtonText.text = "Y";
+    //    }
+    //}
     public void OnXButtonPressed()
     {
         spawnedObject.GetComponent<LeanTwistRotateAxis>().ChangeAxis(new Vector3(-1f, 0f, 0f));
+        XYZText.text = "Current axis: X";
         //Debug.Log(spawnedObject.GetComponent<LeanTwistRotateAxis>().Axis);
     }
     public void OnYButtonPressed()
     {
         spawnedObject.GetComponent<LeanTwistRotateAxis>().ChangeAxis(new Vector3(0f, -1f, 0f));
+        XYZText.text = "Current axis: Y";
         //Debug.Log(spawnedObject.GetComponent<LeanTwistRotateAxis>().Axis);
     }
     public void OnZButtonPressed()
     {
         spawnedObject.GetComponent<LeanTwistRotateAxis>().ChangeAxis(new Vector3(0f, 0f, 1f));
+        XYZText.text = "Current axis: Z";
         //Debug.Log(spawnedObject.GetComponent<LeanTwistRotateAxis>().Axis);
     }
     #endregion
