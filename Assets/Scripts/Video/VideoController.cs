c079d91ca88334e221c77e10ba28375d620525bc
tanmarcus <tanmarcus16@gmail.com>
2023-07-25 13:20:33 +0800

    imported fast forward/backward buttons, implemented video progress bar + timer
    video progress bar can be dragged/tapped on to change video point of time

--- Assets/Scripts/Video/VideoController.cs
+++ Assets/Scripts/Video/VideoController.cs
@@ -1,102 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Video;
 using UnityEngine.Sprites;
 using UnityEngine.UI;
 using TMPro;
 
-public class VideoController : MonoBehaviour
+public class VideoController : MonoBehaviour/*, IDragHandler, IPointerDownHandler*/
 {
-    [SerializeField]private VideoPlayer SsPlayer;
-    //[SerializeField]private VideoPlayer FsPlayer;
-    [SerializeField]private GameObject SsPlayerObj;
-    //[SerializeField]private GameObject FsPlayerObj;
+    [SerializeField]private VideoPlayer videoPlayer;
+    private Texture vidTex;
+    //private Image progressBar;
+
     public Button playPauseButton;
-    public Button SsButton;
-    //public Button FsButton;
     public Sprite startSprite;
     public Sprite pauseSprite;
 
-    public Slider videoSlider;
     public TMP_Text videoTimeText;
-    public float currentVideoTime;
     private void Start()
     {
-        //SsPlayerObj.SetActive(true);
-        //FsPlayerObj.SetActive(false);
-        //SsPlayerObj.SetActive(false);
-        //FsPlayerObj.SetActive(true);
-
-        SsPlayer.Play();
+        videoPlayer.Play();
         playPauseButton.image.sprite = pauseSprite;
+        //Debug.Log("video player prepared: " + videoPlayer.isPrepared);
+        videoPlayer.GetComponent<VideoPlayer>();
+
+        
+        //if (videoPlayer.isPrepared == true)
+        //{
+        //    vidTex = videoPlayer.texture;
+        //    Debug.Log("Vidtex = texture");
+        //}
+
+        //float videoWidth = vidTex.width;
+        //float videoHeight = vidTex.height;
     }
 
     private void Update()
     {
-        videoSlider.value = 1 / (float)SsPlayer.time;
-        videoTimeText.text = SsPlayer.time.ToString("0.00");
+        if (videoPlayer.isPrepared == true)
+        {
+            vidTex = videoPlayer.texture;
+
+            float videoWidth = vidTex.width;
+            float videoHeight = vidTex.height;
+        }
+
+        videoTimeText.text = videoPlayer.time.ToString("0.00");
     }
 
     public void OnSkipBackward()
     {
-        SsPlayer.time -= 10f;
-        //FsPlayer.time -= 10f;
+        videoPlayer.time -= 5f;
     }
 
     public void OnSkipForward()
     {
-        SsPlayer.time += 10f;
-        //FsPlayer.time += 10f;
+        videoPlayer.time += 5f;
     }
     public void OnStartPausePressed()
     {
-        if (SsPlayer.isPlaying == false/* && FsPlayer.isPlaying == false*/)
+        if (videoPlayer.isPlaying == false)
         {
-            SsPlayer.Play();
-            //FsPlayer.Play();
+            videoPlayer.Play();
             playPauseButton.image.sprite = pauseSprite;
         }
         else
         {
-            SsPlayer.Pause();
-            //FsPlayer.Pause();
+            videoPlayer.Pause();
             playPauseButton.image.sprite = startSprite;
         }
-    }
-
-    public void OnSSButtonPressed()
-    {
-        //SsPlayerObj.SetActive(false);
-        //FsPlayerObj.SetActive(true);
-        //FsButton.enabled = true;
-        //FsPlayer.Play();
-        //if (FsPlayer.isActiveAndEnabled == true)
-        //{
-        //    SsPlayer.time = FsPlayer.time;
-        //    FsPlayer.Play();
-        //}
-        
-        Debug.Log("Ss video");
-    }
-
-    public void OnFSButtonPressed()
-    {
-        //SsPlayerObj.SetActive(true);
-        //FsPlayerObj.SetActive(false);
-        SsPlayerObj.transform.position = new Vector3(SsPlayerObj.transform.position.x, SsPlayerObj.transform.position.y, 0);
-        //FsPlayerObj.transform.position = new Vector3(FsPlayerObj.transform.position.x, FsPlayerObj.transform.position.y, 10000);
-        SsButton.enabled = true;
-        //if (SsPlayer.isActiveAndEnabled == true)
-        //{
-        //    FsPlayer.time = SsPlayer.time;
-        //    SsPlayer.Play();
-        //}
-        Debug.Log("Fs video");
-    }
-
-    public void OnSliderMoved()
-    {
-        SsPlayer.time = videoSlider.value / SsPlayer.length;
     }
 }
