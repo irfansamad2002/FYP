71d6b7b973c1b860f2d23c3391877340a2d73507
Irfan Samad <irfansamad2002@gmail.com>
2023-08-04 16:58:00 +0800

    Change buttons in Tool info

---
+++ Assets/Scripts/Main Scene Scripts/ToolInfo Scripts/ToolInfoVideoBtnColor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolInfoVideoBtnColor : MonoBehaviour
+{
+    public Sprite dtVideo;
+    public Sprite dhVideo;
+
+
+    private void OnEnable()
+    {
+         if (ButtonReferenceManager.Instance.storedDTHButtonID == DTHEnum.DH)
+         {
+            GetComponent<Image>().sprite = dhVideo;
+         }
+         else if(ButtonReferenceManager.Instance.storedDTHButtonID == DTHEnum.DT)
+         {
+            GetComponent<Image>().sprite = dtVideo;
+         }
+    }
+}
